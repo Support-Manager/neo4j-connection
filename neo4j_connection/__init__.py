ffdad24473e0573a71f9bd40ab21921a76d8c26f
Linus Bartsch <frenki57a@gmail.com>
2018-08-30 21:23:29 +0200

    added outlaw models to init

--- neo4j_connection/__init__.py
+++ neo4j_connection/__init__.py
@@ -1,2 +1,2 @@
-from .models import UserMixin, GuildMixin, TicketMixin, ResponseMixin
+from .models import UserMixin, GuildMixin, TicketMixin, ResponseMixin, WarningMixin, KickMixin, BanMixin
 from py2neo import Graph
